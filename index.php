2163d3509fc7ae275f155d726a08579106d643d8
Yaksh Sharma <yaksh20548@gmail.com>
2023-11-24 14:21:25 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!--All the meta tags-->
+    </head>
+    <body>
+        <header id="headerindex">
+            <!-- this will be the header-->
+        </header>
+    </body>
+</html>
+<?php   
+?> 
