c49618b89c7d06e32b69780df13f4c3840c566bd
Yaksh Sharma <yaksh20548@gmail.com>
2023-11-30 15:32:23 -0500

    yaksh change another

--- index.php
+++ index.php
@@ -1,46 +1,32 @@
+
+<?php require_once('header.php');?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <!--All the meta tags-->
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
-        <header id="headerindex">
-            <div>
-            <div id="ty">
-                <div id="upperbarindex" class="flexed">
-                <div id="Home">
-                        <button id="homeindex" class="upperbarbuttons"><strong>Home</strong></button>
-                        <button id="searchindex" class="upperbarbuttons"><strong>Search</strong></button>
-                        <button id="signinindex" class="upperbarbuttons"><strong>Sign in</strong></button>
-                    </div>
-                    <div id="nameofthepage">
-                        Bloggers
-                    </div>
-                </div>
-            </div>
-            </div>
-        </header>
         <div id="blogbody">
             <?php
             require_once('abstractDAO.php');
-            class userDAO extends abstractDAO {
-                function __construct() {
-                    try{
-                        parent::__construct();
-                    } catch(mysqli_sql_exception $e){
-                        throw $e;
-                    }
-                }
+            $user = new abstractDAO();
+            $mysqli=$user->getMysqli();
+            $query='SELECT blog_title,blog_data FROM blog;';
+            $blogtitle = mysqli_query($mysqli,$query);
+            if (![$blogtitle]) {
+                die("Query failed: " . mysqli_error($mysqli));
             }
-            $user = new userDAO();
-            $user = $user->__construct();
+            echo "<div class='blogtitlecontainerbig'>";
+            while ($blog = mysqli_fetch_assoc($blogtitle)){
+                echo "<div class='blogtitlecontainersmall'>";
+                echo "<h1 class='blogtitle'>{$blog['blog_title']}</h1>";
+                echo "<button class='blogviewbutton'>view</button>";
+                echo "</div>";
+            }echo "</div>";
+            mysqli_free_result($blogtitle);
             ?>
-            <div class="blog"></div>
-            <div class="blog"></div>
-            <div class="blog"></div>
-            <div class="blog"></div>
-            <div class="blog"></div>
+            
         </div>
     </body>
 </html>
